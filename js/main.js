2bcfe85a9b69bb912e72a3a8451d1d85ac8f7e51
yshu17 <magnificojack@gmail.com>
2023-01-22 17:47:02 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,132 +1,161 @@
 //Burger Menu
-document.getElementById("hamburger").addEventListener("click",()=> {
-  document.getElementById("hamburger").classList.toggle('open');
-  document.getElementById("menu").classList.toggle("change-opacity");
-})
+let burgerBtn = document.getElementById("hamburger");
+let burgerMenu = document.getElementById("menu");
+
+burgerBtn.addEventListener("click", (e) => {
+
+  burgerBtn.classList.toggle('open');
+  burgerMenu.classList.toggle("change-opacity");
+  disableScroll();
+
+  window.addEventListener('click', function (e) {
+
+    if (!e.target.closest(`.${burgerMenu.className}`) &&
+      !e.target.closest(`.${burgerBtn.className}`)) {
+      burgerBtn.classList.toggle('open');
+      burgerMenu.classList.toggle('change-opacity');
+      enableScroll();
+    }
+
+  });
+});
+
+//Scroll-Disabler
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+let keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+let supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch (e) { }
+
+let wheelOpt = supportsPassive ? { passive: false } : false;
+let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
 
 //Plugin Swiper.js
-var swiper = new Swiper(".homes-swiper",
-{
+let swiper = new Swiper(".homes-swiper", {
   speed: 700,
-  slidesPerView: 4, 
+  slidesPerView: 4,
   slidesPerGroup: 1,
   loop: true,
   allowTouchMove: false,
   loopFillGroupWithBlank: true,
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+
   autoplay: {
     delay: 3000,
     pauseOnMouseEnter: true,
   },
+
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
       slidesPerGroup: 1,
       allowTouchMove: true,
-      
     },
-    // when window width is >= 764px
+
     550: {
       slidesPerView: 2,
       slidesPerGroup: 1,
       allowTouchMove: true,
-      
     },
+
     764: {
       slidesPerView: 3,
       slidesPerGroup: 1,
       allowTouchMove: true,
-      
     },
-    // when window width is >= 1300px
+
     1300: {
       slidesPerView: 4,
       slidesPerGroup: 1,
     }
   }
 });
-var swiper2 = new Swiper(".comments-swiper", 
-{
+
+let swiper2 = new Swiper(".comments-swiper", {
   slidesPerView: 2,
-  slidesPerGroup:1,
-  loop:true,
+  slidesPerGroup: 1,
+  loop: true,
   spaceBetween: 50,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      slidesPerGroup:1,
-      
+      slidesPerGroup: 1,
     },
-    // when window width is >= 1300px
+
     700: {
       slidesPerView: 2,
-      slidesPerGroup:1,
+      slidesPerGroup: 1,
     }
   }
 });
 
 //Plugin Leafletjs Map
-var map = L.map('map').setView([48.99006396190157, 37.737753903577996], 16);
+let map = L.map('map').setView([48.99006396190157, 37.737753903577996], 16);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    minZoom: 15,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 17,
+  minZoom: 15,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([48.99006396190157, 37.737753903577996]).addTo(map);
 
-//My Function JS
-//ShowInfoOnMap(document.querySelector(".leaflet-pane"),document.querySelector(".info-box"));
-function ShowInfoOnMap(markerOnMap,infoBox) {
-  let timerId1, timerId2, timerId3, timerId4;
-  markerOnMap.addEventListener("mouseenter", function (e) {
-    clearTimeout(timerId3); clearTimeout(timerId4);
-    let infoBoxCL = infoBox.classList;
-    if (infoBox.style.display == "none") {
-      infoBox.style.display = "inline-block";
-    }
-    infoBox.style.display = "inline-block";
-    infoBoxCL.add("animate__zoomIn");
+let marker = L.marker([48.99006396190157, 37.737753903577996]).addTo(map);
 
-    timerId1 = setTimeout(() => infoBoxCL.replace("animate__zoomIn", "animate__zoomOut"), 2000);
-    timerId2 = setTimeout(() => {
-      infoBoxCL.remove("animate__zoomOut");
-      infoBox.style.display = "none";
-    }, 2500);
+//Scroll-To-Top
+let btn = $('.scroll-top');
 
-  });
-  markerOnMap.addEventListener("click", function () {
-    clearTimeout(timerId1); clearTimeout(timerId2);
-    infoBox.style.display = "inline-block";
-    timerId3 = setTimeout(() => infoBox.classList.add("animate__zoomOut"), 10 * 1000);
-    timerId4 = setTimeout(() => infoBox.style.display = "none", 10 * 2000);
-  });
-}
-
-//My Function on Jq
-var btn = $('.scroll-top');
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
   }
 });
-btn.on('click', function(e) {
+
+btn.on('click', function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $('html, body').animate({ scrollTop: 0 }, '300');
 });
 
 //Plugin Jq PageScroll2id
-$(window).on("load",function(){
-    $("[href*='#']").mPageScroll2id();
-}); 
+$(window).on("load", function () {
+  $("[href*='#']").mPageScroll2id();
+});
 
